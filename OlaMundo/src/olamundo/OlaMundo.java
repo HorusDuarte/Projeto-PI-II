b70baf892519b6de18cef51bfd9e960db2efc737
Ricardo Duarte <49497130+HorusDuarte@users.noreply.github.com>
2021-08-11 21:25:39 -0300

    Sending first project

---
+++ OlaMundo/src/olamundo/OlaMundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package olamundo;
+import java.awt.*;
+import javax.swing.*;
+/**
+ *
+ * @author Ricardo
+ */
+public class OlaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+      new telaMensagem().setVisible(true);
+       
+        
+    }
+    
+}
